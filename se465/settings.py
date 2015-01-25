9596d6906a7a16f2f1678961a2493e626a496499
Patrick Lam <prof.lam@gmail.com>
2015-01-25 10:04:58 -0500

    settings are useful to keep also

---
+++ se465/settings.py
@@ -0,0 +1,98 @@
+"""
+Django settings for in project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ADMINS = (('Patrick Lam', 'prof.lam@gmail.com'))
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_ssh',
+    'django_gitolite',
+    'se465'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'se465.urls'
+
+WSGI_APPLICATION = 'se465.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Toronto'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_ROOT = '/var/www/django-media/'
+MEDIA_URL = 'http://patricklam.ca/django-media/'
+
+# Auth
+LOGIN_URL = '/se465-groups/login/'
+LOGIN_REDIRECT_URL = '/se465-groups/'
+
+# CAS
+AUTHENTICATION_BACKENDS = (
+    'django_cas.backends.CASBackend',
+)
+MIDDLEWARE_CLASSES += ('django_cas.middleware.CASMiddleware',)
+CAS_SERVER_URL = 'https://cas.uwaterloo.ca/cas/'
